3cd04b51db399acf3437e4cb805398cbc20fe77d
Bethany Armitage <bethgrace5@gmail.com>
2018-03-10 15:14:12 -0800

    Adds search component for react

--- smurf/src/App.js
+++ smurf/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Search from './components/search.js'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <search />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ smurf/src/components/search.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+
+const Search = () => (
+    <p>hi</p>
+);
+
+
+/*
+class Search extends Component {
+  render = () => {
+    return (
+        Search;
+        )
+    
+  }
+}
+  */
+
+
+export default Search
+
