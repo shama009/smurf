8d91f83e4109a14fe7c391d149059b09bb65903a
Shama <shamaanjum09@outlook.com>
2018-03-10 16:45:46 -0800

    state for components

--- smurf/src/App.js
+++ smurf/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Search from './components/Search.js'
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
--- smurf/src/components/search.js
+++ smurf/src/components/search.js
@@ -1,10 +1,21 @@
 import React, {Component} from "react"
 
 class Search extends Component {
+
+  state = {
+    searchSkill: ""
+  }
+
+  handleInputChang=(event) =>{
+    const {name, value} = event.target;
+    this.setState({
+      [name] : value
+    });
+  };
   render() {
     return (
       <div>
-        <input type="text" placeholder="skill"/>
+        <input type="text" placeholder="skill" />
         <p>hi</p>
       </div>
     )
