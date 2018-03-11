ffecdd0ef280c5e044939624a4a268bb1906fdde
Bethany Armitage <bethgrace5@gmail.com>
2018-03-10 16:56:42 -0800

    move search to new folder

--- smurf/src/App.js
+++ smurf/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactBootstrap from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
-import Search from './components/Search'
+import Search from './components/search/Search'
 import Header from './components/Header'
 
 class App extends Component {
