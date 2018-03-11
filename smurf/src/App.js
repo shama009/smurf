3ae94f0efd678363d24e1ef178f4c56fb0e1d17b
Bethany Armitage <bethgrace5@gmail.com>
2018-03-10 16:43:52 -0800

    Adds search and navbar

--- smurf/src/App.js
+++ smurf/src/App.js
@@ -2,20 +2,15 @@ import React, { Component } from 'react';
 import ReactBootstrap from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
-import Search from './components/Search.js'
+import Search from './components/Search'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to smurf</h1>
-        </header>
+      <div>
+        <Header />
         <Search />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- smurf/src/components/Search.js
+++ smurf/src/components/Search.js
@@ -1,13 +1,18 @@
 import React, {Component} from "react"
+import { FormControl, Grid, Row, Col } from 'react-bootstrap';
 
 class Search extends Component {
   render() {
     return (
-      <div>
-        <input type="text" placeholder="skill"/>
-        <p>hi</p>
-      </div>
-    )
+        <Grid>
+          <Row>
+          <Col sm={12} md={6} mdOffset={3}>
+          <br />
+          <FormControl type="text" placeholder="I want to learn..." />
+          </Col>
+          </Row>
+        </Grid>
+        )
   }
 };
 
